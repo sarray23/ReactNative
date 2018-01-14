f7d8c583a9c2f350f728e39aad8589ea5d0892d1
macbook <macbook@Devicedemacbook.home>
2018-01-14 18:04:43 +0100

    updates

--- ReactAuthentication/app/components/Login.js
+++ ReactAuthentication/app/components/Login.js
@@ -36,7 +36,7 @@ export default class Login extends Component<{}> {
      }
   }
 
-/*
+
 
 
 //This method will check either my user is already connected or not, in case not, the user would be redirected directly to his home page
@@ -53,7 +53,7 @@ export default class Login extends Component<{}> {
        this.props.navigation.navigate('Profile');
      }
   }
-*/
+
   render() {
     return (
 
